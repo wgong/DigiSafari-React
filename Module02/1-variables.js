b097f37692ebecf9716b6ed7669f2c2927237a13
Ashok kumar <ashok@digi-safari.com>
2024-09-30 20:12:04 +0530

    with let

--- Module02/1-variables.js
+++ Module02/1-variables.js
@@ -1,1 +1,17 @@
-console.log("Welcome to JavaScript") 
+console.log("Welcome to JavaScript")
+// ES5 - var keyword is used to create variables
+
+var myVariable = "My Value"
+console.log(myVariable);
+
+// ES6 - let & const keyworks are introduced with block scope
+
+// A Block in JS
+{
+    let blockVariable = "This is block variable"
+    blockVariable = `modified with 
+                    Multi line
+                    code`
+    console.log(blockVariable);
+}
+// console.log(blockVariable); 

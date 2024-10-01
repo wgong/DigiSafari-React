58afb31bb9bad1ef1847135d1c3d3495c53eff97
Ashok kumar <ashok@digi-safari.com>
2024-10-01 19:05:24 +0530

    props demo app

---
+++ Module05/props-demo-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductDetail from './components/ProductDetail'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Working with Props</h1>
+      <ProductDetail />
+    </>
+  )
+}
+
+export default App

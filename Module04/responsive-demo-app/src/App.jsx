8741bd045b64595644c1b9fa49beab8e6aaf227a
Ashok kumar <ashok@digi-safari.com>
2024-10-01 18:43:10 +0530

    responsive images and video

--- Module04/responsive-demo-app/src/App.jsx
+++ Module04/responsive-demo-app/src/App.jsx
@@ -11,7 +11,23 @@ function App() {
     <div className='container'>
       <Navbar />
       <hr />
-      <h1>Working with Responsive design</h1>
+      <div className='display-3'>Working with Responsive design</div>
+      <div className="row">
+        <div className="col">
+          <img src="/src/assets/images/image1.jpg" className='img-fluid' alt="Image 1" />
+        </div>
+        <div className="col">
+          <img src="/src/assets/images/image2.jpg" className='img-fluid' alt="Image 2" />
+        </div>
+        <div className="col">
+          {/* <video className='ratio ratio-16x9' controls>
+            <source src="/src/assets/videos/sample-video.mp4" type='video/mp4'></source>
+          </video> */}
+          <div className="ratio ratio-4x3">
+              <iframe title='Working with YouTube Videos' src="https://www.youtube.com/embed/5kozt0uDa4c?si=maroeYK0KyupXl4s" allowfullscreen></iframe>
+            </div>
+        </div>
+      </div>
       <hr />
       <Footer />
     </div>

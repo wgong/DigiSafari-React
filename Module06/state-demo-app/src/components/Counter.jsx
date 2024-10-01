b14cfdaa7246bebca6a6e4e00e4bbddfa0b956cd
Ashok kumar <ashok@digi-safari.com>
2024-10-01 23:12:04 +0530

    Local Storage

--- Module06/state-demo-app/src/components/Counter.jsx
+++ Module06/state-demo-app/src/components/Counter.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { useState } from 'react';
 
 function Counter() {
-    const [count, setCount] = useState(0)
+    const getInitialCountFromLocalStorage = () =>{
+      return Number(localStorage.getItem('count') || 0) 
+    }
+
+    const [count, setCount] = useState(getInitialCountFromLocalStorage)
 
     const increment = ()=>{
         setCount((count)=> count + 1)
@@ -11,12 +15,17 @@ function Counter() {
     const incrementByValue = (value)=>{
         setCount((count)=> count + value)
     }
+
+    const saveCountToLocalStorage=()=>{
+      localStorage.setItem("count", count)
+    }
     
   return (
     <div>
         <h2>Current Count: {count}</h2>
         <button onClick={increment}>Increment</button>
         <button onClick={()=>incrementByValue(2)}>Increment by 2</button>
+        <button onClick={saveCountToLocalStorage}>Update Count to Local Storage</button>
     </div>
   )
 }

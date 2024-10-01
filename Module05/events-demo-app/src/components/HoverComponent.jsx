d8a96205b22b007866e77eb3cf6029ac9013feb2
Ashok kumar <ashok@digi-safari.com>
2024-10-01 21:10:33 +0530

    with id passed back to event handler

--- Module05/events-demo-app/src/components/HoverComponent.jsx
+++ Module05/events-demo-app/src/components/HoverComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function HoverComponent({handleHover}) {
+function HoverComponent({handleHover, id}) {
   return (
     <div>
-        <h1 onMouseOver={()=>handleHover("Message from Hover Component")}>Hover to trigger the event handler</h1>
+        <h1 onMouseOver={()=>handleHover(id)}>Hover to trigger the event handler</h1>
     </div>
   )
 }

610468bfac5d61563f26fc1201ecb6971dd4be1a
Ashok kumar <ashok@digi-safari.com>
2024-10-02 18:49:06 +0530

    useState to show error and data on ui

--- Module06/form-demo-app/src/components/Register.jsx
+++ Module06/form-demo-app/src/components/Register.jsx
@@ -1,19 +1,27 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 function Register() {
     const usernameRef = useRef()
     const passwordRef = useRef()
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [formData, setFormData] = useState({username: '', password: ''})
 
     const submitHandler = (event) =>{
         event.preventDefault();
         let username = usernameRef.current.value
         let password = passwordRef.current.value
-        console.log("Register form submitted", username, password);        
+        if(username != '' && password !=''){
+            setErrorMessage(null)
+            console.log("Register form submitted", username, password);
+            setFormData((formData)=> ({...formData, username, password}))
+        }else{
+            setErrorMessage('Username and Password is Required')
+        }                
     }
   return (
     <div>
         <h1>Register form using useRef</h1>
-        {/* {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>} */}
+        {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
         <form onSubmit={submitHandler}>
             <div>
                 <label>Username</label>
@@ -26,10 +34,10 @@ function Register() {
             <button type='submit'>Submit</button>
         </form>
         <p>
-            Username: 
+            Username: {formData.username}
         </p>
         <p>
-            Password: 
+            Password: {formData.password}
         </p>
     </div>
   )
